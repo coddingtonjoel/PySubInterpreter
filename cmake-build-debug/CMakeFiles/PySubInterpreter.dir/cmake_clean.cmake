b3df93926267f095c7fd65cfc40350ae188d80d9
Joel Coddington <coddingtonjoel@gmail.com>
2020-09-03 22:25:27 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/PySubInterpreter.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PySubInterpreter.dir/Interface.cpp.o"
+  "CMakeFiles/PySubInterpreter.dir/main.cpp.o"
+  "PySubInterpreter"
+  "PySubInterpreter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PySubInterpreter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/PySubInterpreter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PySubInterpreter.
+# This may be replaced when dependencies are built.
